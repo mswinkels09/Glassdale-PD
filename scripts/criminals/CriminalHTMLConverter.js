f14de7a5b85bc6f6cf42145b5c43936ba8e6993b
mswinkels09 <mswinkels09@gmail.com>
2020-08-14 15:13:23 -0500

    added alibi button and close alibi button to DOM

--- scripts/criminals/CriminalHTMLConverter.js
+++ scripts/criminals/CriminalHTMLConverter.js
@@ -1,3 +1,23 @@
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("criminal__alibiButton")) {
+        const [prompt, criminalId] = clickEvent.target.id.split("--")
+
+        const alibiEvent = new CustomEvent("associatesClicked", {
+            detail: {
+                chosenCriminal: criminalId
+            }
+        })
+
+        eventHub.dispatchEvent(alibiEvent)
+    } else if (clickEvent.target.id === "associateCloseButton") {
+        const modalElement = clickEvent.target.parentNode
+        modalElement.close()
+    }
+})
+
 export const CriminalHTMLConverter = (criminalObj) => {
     return `
     <article class="criminal__info">
@@ -6,6 +26,7 @@ export const CriminalHTMLConverter = (criminalObj) => {
         <section class="criminal__crime">Crime: ${criminalObj.conviction}</section>
         <section class="criminal__termStart">Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</section>
         <section class="criminal__termEnd">Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</section>
+        <button id="criminal__alibiButton--${criminalObj.id}">Alibis</button>
     </article>
 `
 }
--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -1,7 +1,8 @@
-export const NoteHTMLConverter = (noteObject) => {
+export const NoteHTMLConverter = (noteObject, criminalObject) => {
     return `
         <section class="note">
             <div class="note--title"><strong> ${ noteObject.title }</strong></div>
+            <div class="note--criminal">Criminal: ${ criminalObject.name }</div>
             <div class="note--content">${ noteObject.content }</div>
             <div class="note--author">Author: ${ noteObject.author }</div>
             <div class="note--timestamp">Timestamp: ${ new Date(noteObject.timestamp).toLocaleDateString('en-US')  }</div>
